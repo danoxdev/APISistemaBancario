030749f8f423d46a0ddea277d51b972f94776457
Daniel D'Onofrio <danieldonofrio@live.com>
2024-12-11 23:37:45 -0300

    actualizacion de testeos unitarios

--- src/test/java/ar/edu/utn/frbb/tup/presentacion/clientes/TestControladorBuscarClientePorDni.java
+++ src/test/java/ar/edu/utn/frbb/tup/presentacion/clientes/TestControladorBuscarClientePorDni.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+
 class TestControladorBuscarClientePorDni {
 
     @InjectMocks
